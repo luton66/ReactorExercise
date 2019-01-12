78ede275093f7eb42f69d2f32c40327de2a65beb
Unknown <luton66@gmail.com>
2019-01-12 22:21:48 +0000

    Initial Commit - No Code

---
+++ src/main/java/exercise/learning/ReactorExercise/ReactorExerciseApplication.java
@@ -0,0 +1,14 @@
+package exercise.learning.ReactorExercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorExerciseApplication.class, args);
+	}
+
+}
+
